0bd4d3fbb21a2bc24f24932d3e145f2af6052f30
hkdas5464 <hkdas5464@gmail.com>
2024-05-13 22:20:05 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  reactStrictMode: true, 
+  images: {
+    formats: ["image/avif", "image/webp"],
+    domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
